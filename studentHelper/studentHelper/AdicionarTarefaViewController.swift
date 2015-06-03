891b5d071ddf041a0cf0fb4d26442703d350d0c8
gabrielAlbertoJesusPreto <gabriel_alberto2010@hotmail.com>
2015-06-03 17:14:33 -0300

    Tela de Adicionar Tarefa OK e algumas mudanças no Storyboard

---
+++ studentHelper/studentHelper/AdicionarTarefaViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AdicionarTarefaViewController.swift
+//  studentHelper
+//
+//  Created by Gabriel Alberto de Jesus Preto on 03/06/15.
+//  Copyright (c) 2015 Humberto  Julião. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AdicionarTarefaViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //backBarButtonItem.color = UIColor.whiteColor()
+     
+    }
+    
+    @IBAction func adicionarTarefa(sender: AnyObject) {
+    }
+}
